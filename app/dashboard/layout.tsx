416f4fa91147869cb249d2aae29152476ead7223
JasonEvan <7ason3van@gmail.com>
2025-07-21 13:05:46 +0700

    style: making sidebar dashboard layout for navigation
    making sidebar that contain some menu for navigation.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,15 @@
+import Sidebar from "@/components/Sidebar";
+import { Box } from "@mui/material";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      {/* sidebar */}
-      {children}
-    </>
+    <Box sx={{ display: "flex", flexDirection: "row", padding: 2 }}>
+      <Sidebar />
+      <Box sx={{ flexGrow: 10, p: 2 }}>{children}</Box>
+    </Box>
   );
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import HomeIcon from "@mui/icons-material/Home";
+import FormatListBulletedAddIcon from "@mui/icons-material/FormatListBulletedAdd";
+import PaymentIcon from "@mui/icons-material/Payment";
+import PaymentsIcon from "@mui/icons-material/Payments";
+import EditIcon from "@mui/icons-material/Edit";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import AssignmentReturnIcon from "@mui/icons-material/AssignmentReturn";
+import BackupIcon from "@mui/icons-material/Backup";
+import ReceiptIcon from "@mui/icons-material/Receipt";
+import { usePathname } from "next/navigation";
+import { Box, Button, Typography } from "@mui/material";
+
+const menus = [
+  { label: "Home", path: "/dashboard", icon: <HomeIcon /> },
+  {
+    label: "Tambah",
+    path: "/dashboard/tambah",
+    icon: <FormatListBulletedAddIcon />,
+  },
+  { label: "Transaksi", path: "/dashboard/transaksi", icon: <PaymentIcon /> },
+  { label: "Pelunasan", path: "/dashboard/pelunasan", icon: <PaymentsIcon /> },
+  { label: "Edit", path: "/dashboard/edit", icon: <EditIcon /> },
+  { label: "Lihat", path: "/dashboard/lihat", icon: <ListAltIcon /> },
+  { label: "Retur", path: "/dashboard/retur", icon: <AssignmentReturnIcon /> },
+  { label: "Backup", path: "/dashboard/backup", icon: <BackupIcon /> },
+  { label: "Print", path: "/dashboard/print", icon: <ReceiptIcon /> },
+];
+
+export default function Sidebar() {
+  const pathName = usePathname();
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        borderRight: "1px solid #ccc",
+        height: "100vh",
+        padding: 2,
+      }}
+    >
+      <Typography variant="h5" gutterBottom>
+        Sejahtera Abadi
+      </Typography>
+      {menus.map((menu) => (
+        <Button
+          key={menu.label}
+          variant={pathName === menu.path ? "contained" : "text"}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+            textAlign: "left",
+          }}
+          startIcon={menu.icon}
+          href={menu.path}
+        >
+          {menu.label}
+        </Button>
+      ))}
+    </Box>
+  );
+}
