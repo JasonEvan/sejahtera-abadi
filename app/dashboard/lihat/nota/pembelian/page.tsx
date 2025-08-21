e80073df2c3b5c9413e249e08fabc502357a19dd
JasonEvan <7ason3van@gmail.com>
2025-08-21 11:44:47 +0700

    feat: add `lihat nota pembelian` feature

--- app/dashboard/lihat/nota/pembelian/page.tsx
+++ app/dashboard/lihat/nota/pembelian/page.tsx
@@ -1,8 +1,54 @@
+"use client";
+
+import FormNota from "@/components/lihat/nota/FormNota";
+import NotaTable from "@/components/lihat/nota/NotaTable";
+import { DetailTransaksiTableRow } from "@/lib/types";
+import { Box, Typography } from "@mui/material";
+import { useState } from "react";
+import Swal from "sweetalert2";
+
 export default function NotaPembelianPage() {
+  const [data, setData] = useState<DetailTransaksiTableRow[]>([]);
+  const [total, setTotal] = useState<string>("0");
+
+  const handleSubmit = async (nomorNota: string) => {
+    const params = {
+      nomornota: nomorNota,
+    };
+
+    const queryParams = new URLSearchParams(params);
+
+    const response = await fetch(
+      `/api/nota/pembelian/lihat?${queryParams.toString()}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const { data, totalHargaSemua } = await response.json();
+    setData(data);
+    setTotal(totalHargaSemua);
+  };
+
+  const handleError = (error: string) => {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: error,
+      confirmButtonText: "OK",
+    });
+    setData([]);
+    setTotal("0");
+  };
+
   return (
-    <div>
-      <h1>Nota Pembelian</h1>
-      {/* Additional content for Nota Pembelian can be added here */}
-    </div>
+    <Box>
+      <Typography variant="h6" gutterBottom>
+        Nota Pembelian
+      </Typography>
+      <FormNota handleSubmit={handleSubmit} handleError={handleError} />
+      <NotaTable data={data} totalHargaSemua={total} />
+    </Box>
   );
 }
