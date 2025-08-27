5a7c557a8cb560706f32836e8b21a7bf5d971611
JasonEvan <7ason3van@gmail.com>
2025-08-27 12:19:51 +0700

    feat: add `print piutang per langganan` feature

--- app/dashboard/lihat/piutang/langganan/page.tsx
+++ app/dashboard/lihat/piutang/langganan/page.tsx
@@ -3,15 +3,27 @@
 import FormLangganan from "@/components/lihat/piutang/FormLangganan";
 import PiutangLanggananTable from "@/components/lihat/piutang/PiutangLanggananTable";
 import { Box, Typography } from "@mui/material";
+import { useState } from "react";
 
 export default function PiutangPerLanggananPage() {
+  const [langganan, setLangganan] = useState<string>("");
+
   return (
     <Box>
-      <Typography variant="h6" gutterBottom>
+      <style>{`
+        @media print {
+          @page {
+            size: A4 portrait;
+            margin: 0;
+          }
+        }
+      `}</style>
+
+      <Typography variant="h6" gutterBottom sx={{ displayPrint: "none" }}>
         Piutang Langganan
       </Typography>
-      <FormLangganan />
-      <PiutangLanggananTable />
+      <FormLangganan setLangganan={setLangganan} />
+      <PiutangLanggananTable langganan={langganan} />
     </Box>
   );
 }
