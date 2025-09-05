09a4240bb2e2539b9ec36fab772f4bff31f8a046
JasonEvan <7ason3van@gmail.com>
2025-09-05 16:45:56 +0700

    feat: add confirmation before submit on retur and edit retur

--- components/retur/NoteSummary.tsx
+++ components/retur/NoteSummary.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Grid, InputAdornment, TextField } from "@mui/material";
+import Swal from "sweetalert2";
 
 export default function NoteSummary({
   diskon,
@@ -13,6 +14,22 @@ export default function NoteSummary({
   isLoading: boolean;
   submitRetur: () => Promise<void>;
 }) {
+  const handleSubmit = async () => {
+    const result = await Swal.fire({
+      title: "Are you sure?",
+      text: "This action cannot be reverted.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, I'm sure!",
+    });
+
+    if (!result.isConfirmed) return;
+
+    await submitRetur();
+  };
+
   return (
     <Box>
       <Grid container spacing={4} marginBottom={4}>
@@ -66,7 +83,11 @@ export default function NoteSummary({
           />
         </Grid>
         <Grid size={{ xs: 12, sm: 6, md: 6, lg: 6 }}>
-          <Button variant="contained" onClick={submitRetur} loading={isLoading}>
+          <Button
+            variant="contained"
+            onClick={handleSubmit}
+            loading={isLoading}
+          >
             Submit
           </Button>
         </Grid>
