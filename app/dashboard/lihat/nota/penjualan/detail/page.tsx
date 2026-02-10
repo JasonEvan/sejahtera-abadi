4bb69c27a546c387f2c8a7a25aa6298461b3be32
JasonEvan <7ason3van@gmail.com>
2026-02-10 13:30:41 +0700

    fix: fix add page bug that jumps 1 page over

--- app/dashboard/lihat/nota/penjualan/detail/page.tsx
+++ app/dashboard/lihat/nota/penjualan/detail/page.tsx
@@ -3,8 +3,30 @@
 import NotaTable from "@/components/lihat/nota/NotaTable";
 import { useDetailNotaStore } from "@/hooks/useDetailNotaStore";
 import { rupiahToString } from "@/lib/rupiahToString";
+import { DetailTransaksiTableRow } from "@/lib/types";
 import { Box, Button } from "@mui/material";
 import jsPDF from "jspdf";
+
+function drawHeader(
+  pdf: jsPDF,
+  y: number = 7,
+  details: DetailTransaksiTableRow[],
+) {
+  pdf.text("SA", 5, y);
+  pdf.text(details[0].tanggal_nota, 80, y);
+  pdf.text("KEPADA YTH", 105, y);
+  pdf.text(details[0].nomor_nota, 80, y + 3);
+  pdf.text(details[0].nama_client, 105, y + 3);
+  pdf.text(details[0].kode_sales, 80, y + 6);
+  const address = details[0].alamat_client
+    ? `${details[0].alamat_client}${
+        details[0].kota_client ? ", " + details[0].kota_client : ""
+      }`
+    : details[0].kota_client || "";
+  pdf.text(address, 105, y + 6);
+
+  return y + 16; // Return new y position after header
+}
 
 export default function NotaPenjualanPage() {
   const { details, total } = useDetailNotaStore();
@@ -17,26 +39,17 @@ export default function NotaPenjualanPage() {
     pdf.setFont("helvetica", "bold");
     pdf.setFontSize(6.3);
 
+    let needToChangePage = false;
+
     // Add header
-    pdf.text("SA", 5, 7);
-    pdf.text(details[0].tanggal_nota, 80, 7);
-    pdf.text("KEPADA YTH", 105, 7);
-    pdf.text(details[0].nomor_nota, 80, 10);
-    pdf.text(details[0].nama_client, 105, 10);
-    pdf.text(details[0].kode_sales, 80, 13);
-    const address = details[0].alamat_client
-      ? `${details[0].alamat_client}${
-          details[0].kota_client ? ", " + details[0].kota_client : ""
-        }`
-      : details[0].kota_client || "";
-    pdf.text(address, 105, 13);
+    let y = 7;
+    y = drawHeader(pdf, y, details);
 
     // Table headers
     const colX = [5, 10, 80, 90, 110, 125];
     const columnWidths = [5, 70, 10, 20, 15, 15];
     const headers = ["No", "Nama Barang", "Qty", "Satuan", "Harga", "Total"];
     const numericalCols = [0, 2, 4, 5];
-    let y = 23;
     headers.forEach((header, i) => {
       // Align right for numerical columns
       // -1 for padding
@@ -69,25 +82,20 @@ export default function NotaPenjualanPage() {
       y += 4;
       rowCount++;
 
-      if (rowCount % 15 === 0 && index < details.length - 1) {
-        pdf.addPage();
+      if (rowCount % 15 === 0 && index < details.length - 2) {
+        if (needToChangePage) {
+          pdf.addPage();
+          y = 7;
+          needToChangePage = false;
+        } else {
+          y += 28;
+          needToChangePage = true;
+        }
 
         // Redraw header
-        pdf.text("SA", 5, 7);
-        pdf.text(details[0].tanggal_nota, 80, 7);
-        pdf.text("KEPADA YTH", 105, 7);
-        pdf.text(details[0].nomor_nota, 80, 10);
-        pdf.text(details[0].nama_client, 105, 10);
-        pdf.text(details[0].kode_sales, 80, 13);
-        const address = details[0].alamat_client
-          ? `${details[0].alamat_client}${
-              details[0].kota_client ? ", " + details[0].kota_client : ""
-            }`
-          : details[0].kota_client || "";
-        pdf.text(address, 105, 13);
+        y = drawHeader(pdf, y, details);
 
         // Redraw table headers
-        y = 23;
         headers.forEach((header, i) => {
           const x = numericalCols.includes(i)
             ? colX[i] + columnWidths[i] - pdf.getTextWidth(header) - 1
